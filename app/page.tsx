1b81fdea09ae41cf37c2fbf99e34a8b27722226e
Satyam Sharan <satyam.sharan@gmail.com>
2023-11-05 06:13:52 +0100

    Removed lint issue

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default function Page() {
         <div className='text-center mt-[10px]'>
           <div className={`cursor-pointer text-white text-xs lg:text-base`} onClick={()=>setShowInfoState(showInfoState==1?2:1)}>Lorenz Attractor</div>
           <div className={(showInfoState==0?`hidden `:``)+(showInfoState==1?`fadeIn `:`fadeOut `)+` p-3 text-center rounded-lg border border-transparent transition-colors border-neutral-50 bg-neutral-50 text-right`}>
-            <button className='text-black close' onClick={()=>setShowInfoState(2)}>&#9587;</button>
-            <p className='text-xs text-black max-w-xs lg:max-w-lg text-justify'>The Lorenz system is a system of ordinary differential equations first studied by mathematician and meteorologist Edward Lorenz. It is notable for having chaotic solutions for certain parameter values and initial conditions. In particular, the Lorenz attractor is a set of chaotic solutions of the Lorenz system. In popular media the "butterfly effect" stems from the real-world implications of the Lorenz attractor, namely that several different initial chaotic conditions evolve in phase space in a way that never repeats, so all chaos is unpredictable. </p>
+            <button className='text-black close' onClick={()=>setShowInfoState(2)}> &#x2573; </button>
+            <p className='text-xs text-black max-w-xs lg:max-w-lg text-justify'>The Lorenz system is a system of ordinary differential equations first studied by mathematician and meteorologist Edward Lorenz. It is notable for having chaotic solutions for certain parameter values and initial conditions. In particular, the Lorenz attractor is a set of chaotic solutions of the Lorenz system. In popular media the &quot;butterfly effect&quot; stems from the real-world implications of the Lorenz attractor, namely that several different initial chaotic conditions evolve in phase space in a way that never repeats, so all chaos is unpredictable. </p>
             <p className='mt-2  text-xs text-black font-bold max-w-xs lg:max-w-lg'><a target="_blank" href='https://en.wikipedia.org/wiki/Lorenz_system'>Read more here</a></p>
           </div>
         </div>

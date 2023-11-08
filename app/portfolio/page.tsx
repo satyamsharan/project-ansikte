22ae6dfdcbbf023d86d0c17ecabe1c66fc36c255
Satyam Sharan <satyam.sharan@gmail.com>
2023-11-08 23:43:41 +0100

    Started on portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,21 +2,13 @@ import Image from "next/image";
 
 export default function Page() {
     return(
-        <div className="pt-[52px] lg:pt-[64px] flex-col justify-center items-center fade-in">
-            
-            <Image className="opacity-20 m-auto" src={'/contact.gif'} alt="" width={300} height={225}/>
-            <div className="text-center mt-[-150px] mb-[20px]" >Portfolio</div>
         
-            <div className="p-4 m-auto text-xs text-justify max-w-[400px]">
-            </div>
-
-            <div className="flex-row lg:flex justify-center items-center">
-                <div className="m-auto lg:m-0 max-w-[360px] p-[10px]">
-                    
-                </div>
-                <div className="m-auto lg:m-0 max-w-[360px] p-[10px] pb-[100px]">
-                    
-                </div>
+        <div className="pt-[52px] lg:pt-[64px] flex-col justify-center items-center fade-in">
+            <Image className="opacity-20 m-auto" src={'/contact.gif'} alt="" width={300} height={225}/>
+            <div className="text-center z-10 mt-[-150px] mb-[20px] relative" >Portfolio</div>
+        
+            <div className="p-4 m-auto text-xs text-justify z-10 max-w-[400px] relative">
+                This portfolio represents my professional evolution, featuring a comprehensive overview of the organizations I've been a part of, my project contributions, notable achievements, as well as my interests and hobbies.
             </div>
         </div>
     );
